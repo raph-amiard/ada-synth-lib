333b6b7e4d2f4660fe50c85411471132b06d33b4
Raphael Amiard <amiard@adacore.com>
2016-02-23 23:00:41 +0100

    Correct typo in Write_To_Stdout

--- src/write_to_stdout.adb
+++ src/write_to_stdout.adb
@@ -1,9 +1,11 @@
 with Utils; use Utils;
 with GNAT.OS_Lib;
+with Interfaces; use Interfaces;
 
 procedure Write_To_Stdout (G : access Generator'Class)
 is
-   function Sample_To_Uint16 is new Sample_To_Int (Short_Integer);
+   function Sample_To_Int16 is new Sample_To_Int (Short_Integer);
+   function Sample_To_UInt16 is new Sample_To_UInt (Unsigned_16);
    Int_Smp : Short_Integer := 0;
    Ignore : Integer;
 begin
@@ -13,7 +15,7 @@ begin
       G.Next_Samples;
 
       for I in B_Range_T'Range loop
-         Int_Smp := Sample_To_Uint16 (G.Buffer (I));
+         Int_Smp := Sample_To_Int16 (G.Buffer (I));
          Ignore := GNAT.OS_Lib.Write
            (GNAT.OS_Lib.Standout, Int_Smp'Address, Int_Smp'Size / 8);
       end loop;
@@ -21,4 +23,6 @@ begin
       exit when Sample_Nb > 10_000_000;
       Sample_Nb := Sample_Nb + Generator_Buffer_Length;
    end loop;
+
+
 end Write_To_Stdout;
