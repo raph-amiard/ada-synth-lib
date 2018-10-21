2e88da57640ffd566eb5fdd27bc66a43cc8d4754
Raphaël AMIARD <raph.amiard@gmail.com>
2018-10-21 13:09:47 +0200

    Comments

--- examples/soundio_example.adb
+++ examples/soundio_example.adb
@@ -12,6 +12,7 @@ procedure Soundio_Example is
    Out_Stream           : access SoundIo_Out_Stream;
    Err                  : SoundIo_Error;
 begin
+   --  SoundIO connection boilerplate
    Err := Connect (IO);
    Flush_Events (IO);
    Default_Device_Index := Default_Output_Device_Index (IO);
@@ -19,6 +20,8 @@ begin
    Out_Stream := Outstream_Create (Device);
    Out_Stream.Format := Format_Float32NE;
    Out_Stream.Write_Callback := Soundio_Output.Write_Callback'Access;
+
+   --  Set the main generator for the audio stream.
    Set_Generator (Out_Stream => Out_Stream, G => Simple_Demo.Main_Mixer);
    Err := Outstream_Open (Out_Stream);
    Err := Outstream_Start (Out_Stream);
