a0095f6ebb14ec78e56da6536e8f56531fbc820a
Raphaël AMIARD <raph.amiard@gmail.com>
2020-05-28 16:24:26 +0200

    Port audio_2 to soundio_example, add stdout backend (#11)

--- examples/soundio_example.adb
+++ examples/soundio_example.adb
@@ -8,6 +8,7 @@ with Soundio;              use Soundio;
 with Soundio_Output;       use Soundio_Output;
 
 with ASL_Examples;
+with Write_To_Stdout;
 
 procedure Soundio_Example is
 
@@ -15,16 +16,23 @@ procedure Soundio_Example is
       Parser : Argument_Parser := Create_Argument_Parser
         (Help => "Run an example for ada-synth-lib");
 
-      type Example_Kind is (Full_Demo);
+      type Example_Kind is (Full_Demo, Full_Demo_2);
+      type Backend_Kind is (Pulse_Audio, Stdout);
 
       package Example is new Parse_Enum_Option
         (Parser,
          "-e", "--example", "Which example to run",
          Arg_Type    => Example_Kind, Default_Val => Full_Demo);
 
+      package Backend is new Parse_Enum_Option
+        (Parser,
+         "-b", "--backend", "Which backend to use",
+         Arg_Type    => Backend_Kind, Default_Val => Pulse_Audio);
+
       function Generator return Generator_Access is
         (case Arg.Example.Get is
-            when Full_Demo => ASL_Examples.Full_Demo.Main_Mixer);
+            when Full_Demo => ASL_Examples.Full_Demo.Main_Mixer,
+            when Full_Demo_2 => ASL_Examples.Full_Demo_2.Main_Mixer);
    end Arg;
 
    IO                   : constant access Soundio.SoundIo := Create;
@@ -35,32 +43,36 @@ procedure Soundio_Example is
 
 begin
    if Arg.Parser.Parse then
+      case Arg.Backend.Get is
+      when Arg.Pulse_Audio =>
+         --  SoundIO connection boilerplate
+         Dummy := Connect (IO);
+         Flush_Events (IO);
+         Default_Device_Index := Default_Output_Device_Index (IO);
+         Device := Get_Output_Device (IO, Default_Device_Index);
+         Out_Stream := Outstream_Create (Device);
+         Out_Stream.Format := Format_Float32NE;
+         Out_Stream.Write_Callback := Soundio_Output.Write_Callback'Access;
 
-      --  SoundIO connection boilerplate
-      Dummy := Connect (IO);
-      Flush_Events (IO);
-      Default_Device_Index := Default_Output_Device_Index (IO);
-      Device := Get_Output_Device (IO, Default_Device_Index);
-      Out_Stream := Outstream_Create (Device);
-      Out_Stream.Format := Format_Float32NE;
-      Out_Stream.Write_Callback := Soundio_Output.Write_Callback'Access;
+         --  Set the main generator for the audio stream.
+         Set_Generator
+           (Out_Stream => Out_Stream, G => Arg.Generator);
+         Dummy := Outstream_Open (Out_Stream);
+         Dummy := Outstream_Start (Out_Stream);
 
-      --  Set the main generator for the audio stream.
-      Set_Generator
-        (Out_Stream => Out_Stream, G => Arg.Generator);
-      Dummy := Outstream_Open (Out_Stream);
-      Dummy := Outstream_Start (Out_Stream);
+         Put_Line ("Backend used : " & IO.current_backend'Img);
 
-      Put_Line ("Backend used : " & IO.current_backend'Img);
+         Play (Out_Stream);
+         loop
+            Wait_Events (IO);
+         end loop;
 
-      Play (Out_Stream);
-      loop
-         Wait_Events (IO);
-      end loop;
-
-      pragma Warnings (Off, "Unreachable");
-      Outstream_Destroy (Out_Stream);
-      Device_Unref (Device);
-      Destroy (IO);
+         pragma Warnings (Off, "Unreachable");
+         Outstream_Destroy (Out_Stream);
+         Device_Unref (Device);
+         Destroy (IO);
+      when Arg.Stdout =>
+         Write_To_Stdout (Arg.Generator);
+      end case;
    end if;
 end Soundio_Example;
